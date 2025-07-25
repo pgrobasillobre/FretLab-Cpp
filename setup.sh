fff0c0d57a86c830efd5335539534784736b3059
Pablo Grobas Illobre <pgrobasillobre@gmail.com>
2025-07-25 09:31:49 +0200

    Setting up setup.sh

---
+++ setup.sh
@@ -0,0 +1,60 @@
+#!/bin/sh
+# Setup script for building FretLabCPP
+
+README=README.md
+ompvar=0
+build="build"
+
+# Parse CLI options
+while test $# -gt 0; do
+    case "$1" in
+        -h|--help)
+            echo "Usage: ./setup.sh [options]"
+            echo "Options:"
+            echo "  -b, --build <dir>    Build directory name (default: build)"
+            echo "  -omp, --omp          Enable OpenMP"
+            exit 0
+            ;;
+        -b|--build)
+            shift
+            build=$1
+            shift
+            ;;
+        -omp|--omp)
+            shift
+            ompvar=1
+            ;;
+        *)
+            break
+            ;;
+    esac
+done
+
+# Set final build directory
+buildir=$build
+
+# Create and enter build directory
+mkdir -p "$buildir"
+cd "$buildir"
+
+# Compose cmake arguments
+cmake_args="-DENABLE_AUTO_BLAS=ON -DENABLE_AUTO_LAPACK=ON"
+if [ $ompvar -eq 1 ]; then
+    cmake_args="$cmake_args -DENABLE_OMP=ON"
+fi
+
+# Run cmake
+cmake .. $cmake_args
+
+# Final user instructions
+echo ""
+echo "✅ CMake configuration complete."
+echo "To compile:"
+echo "$ cd $buildir"
+echo "$ make -j\$(nproc)"
+echo ""
+echo "To test:"
+echo "$ cd $buildir"
+echo "$ ctest"
+echo ""
+
